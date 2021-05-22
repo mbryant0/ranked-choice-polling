d8f4fa676566509b212d155ed46be623f5ea9c5c
Mikayla Bryant <mikaylabryant25@gmail.com>
2021-05-21 21:58:17 -0400

    Added frontend back to repo

---
+++ polling-app/scenes/login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, Text, TouchableHighlight } from 'react-native';
+
+const LoginScreen = ({ navigation }) => {
+  <SafeAreaView>
+    <Text>Screen: Login</Text>
+    <TouchableHighlight>
+      <Text>Go to home</Text>
+    </TouchableHighlight>
+  </SafeAreaView>;
+};
+
+export default LoginScreen;
